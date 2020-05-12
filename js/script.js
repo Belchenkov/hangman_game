5e3960a6634e5775eed7c3676f50e2f4c49f6648
Aleksey Belchenkov <u608110@gmail.com>
2020-05-12 14:09:05 +0300

    Wrong Letters

--- js/script.js
+++ js/script.js
@@ -33,7 +33,25 @@ function displayWord() {
 }
 
 function updateWrongLettersEl() {
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
 
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length;
+        if (index < errors) {
+            part.style.display = 'block';
+        } else {
+            part.style.display = 'none';
+        }
+    });
+
+    // Check if lost
+    if (wrongLetters.length === figureParts.length) {
+        finalMessage.innerText = 'Unfortunately you lost.';
+        popup.style.display = 'flex';
+    }
 }
 
 function showNotification() {
